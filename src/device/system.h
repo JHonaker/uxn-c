b40f1baa0a15b4d9033d93e01a9b410de3d5c37d
John Honaker <honaker215@gmail.com>
2024-11-14 14:57:46 -0500

    Add system device and hardcode op test rom loading.

---
+++ src/device/system.h
@@ -0,0 +1,24 @@
+#include "../common.h"
+#include "../uxn.h"
+
+#define SYSTEM_EXPANSION_PORT 0x02
+#define SYSTEM_WST_PORT 0x04
+#define SYSTEM_RST_PORT 0x05
+#define SYSTEM_METADATA_PORT 0x06
+#define SYSTEM_RED_PORT 0x08
+#define SYSTEM_GREEN_PORT 0x0a
+#define SYSTEM_BLUE_PORT 0x0c
+#define SYSTEM_DEBUG_PORT 0x0e
+#define SYSTEM_STATE_PORT 0x0f
+
+typedef enum ExpansionOp {
+  FILL, CPYL, CPYR
+} ExpansionOp;
+
+void system_reboot(Uxn* uxn, char* rom_path, bool soft_reboot);
+void system_inspect(Uxn* uxn);
+int system_error(char* msg, const char* err);
+int system_boot(Uxn* uxn, char* rom_path);
+
+Byte system_dei(Uxn* uxn, Byte addr);
+void system_deo(Uxn* uxn, Byte addr);
--- src/main.c
+++ src/main.c
@@ -2,30 +2,15 @@
 
 #include "common.h"
 #include "uxn.h"
+#include "device/system.h"
 
 int main(int argc, const char* argv[]) {
   Uxn* uxn = Uxn_new();
 
-  Short pc = RESET_VECTOR;
-
-  Uxn_push_work(uxn, 0x80);
-  Uxn_push_ret(uxn, 0x80);
-
-  printf("\nBefore pop\n\n");
-
-  Uxn_dump(uxn);
-
-  printf("\nAfter pop\n\n");
-  Uxn_pop_work(uxn);
-
-  Uxn_dump(uxn);
-
-  Uxn_pop_ret(uxn);
-
-  Uxn_mem_write(uxn, RESET_VECTOR, 0x00);
+  system_boot(uxn, "./opctest.rom");  
 
   Uxn_eval(uxn, RESET_VECTOR);
-  printf("\n After eval! \n\n");
+  
 
   Uxn_delete(uxn);
 
@@ -33,9 +18,17 @@ int main(int argc, const char* argv[]) {
 }
 
 Byte Uxn_dei_dispatch(Uxn* uxn, Byte addr) {
-  return Uxn_dev_read(uxn, addr);
+  const Byte page = addr & 0x0f;
+  switch (page) {
+    case 0x00: return system_dei(uxn, addr);
+    default: return Uxn_dev_read(uxn, addr);
+  }
 }
 
 void Uxn_deo_dispatch(Uxn* uxn, Byte addr) {
-  return;
+  const Byte page = addr & 0x0f;
+  switch (page) {
+    case 0x00: return system_deo(uxn, addr);
+    default: return;
+  }
 }
