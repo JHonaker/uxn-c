08823792a817e1a3c805a024faddb3c8de3533a7
John Honaker <honaker215@gmail.com>
2024-11-15 00:21:33 -0500

    Rename CONSOLE_ADDR_x macros to CONSOLE_x_PORT for consistency with other devices.

--- src/device/console.c
+++ src/device/console.c
@@ -5,34 +5,34 @@
 #include <stdio.h>
 
 int console_input_event(Uxn* uxn, Byte c, Byte type) {
-  Uxn_dev_write(uxn, CONSOLE_ADDR_READ, c);
-  Uxn_dev_write(uxn, CONSOLE_ADDR_TYPE, type);
+  Uxn_dev_write(uxn, CONSOLE_READ_PORT, c);
+  Uxn_dev_write(uxn, CONSOLE_TYPE_PORT, type);
 
-  Byte high = Uxn_dev_read(uxn, CONSOLE_ADDR_ADDR);
-  Byte low = Uxn_dev_read(uxn, CONSOLE_ADDR_ADDR + 1);
+  Byte high = Uxn_dev_read(uxn, CONSOLE_ADDR_PORT);
+  Byte low = Uxn_dev_read(uxn, CONSOLE_ADDR_PORT + 1);
   Short vector_addr = (high << 8) | low;
 
   return Uxn_eval(uxn, vector_addr);
 }
 
 void console_display_write(Uxn* uxn) {
-  Byte write_byte = Uxn_dev_read(uxn, CONSOLE_ADDR_WRITE);
+  Byte write_byte = Uxn_dev_read(uxn, CONSOLE_WRITE_PORT);
   fputc(write_byte, stdout);
   fflush(stdout);
 }
 
 void console_display_error(Uxn* uxn) {
-  Byte error_byte = Uxn_dev_read(uxn, CONSOLE_ADDR_ERROR);
+  Byte error_byte = Uxn_dev_read(uxn, CONSOLE_ERROR_PORT);
   fputc(error_byte, stderr);
   fflush(stderr);
 }
 
 void console_deo(Uxn* uxn, Byte addr) {
   switch (addr) {
-    case CONSOLE_ADDR_WRITE:
+    case CONSOLE_WRITE_PORT:
       console_display_write(uxn);
       break;
-    case CONSOLE_ADDR_ERROR:
+    case CONSOLE_ERROR_PORT:
       console_display_error(uxn);
       break;
     default:
--- src/device/console.h
+++ src/device/console.h
@@ -1,14 +1,14 @@
 #include "../common.h"
 #include "../uxn.h"
 
-#define CONSOLE_ADDR_VECTOR 0x10
-#define CONSOLE_ADDR_READ 0x12
-#define CONSOLE_ADDR_TYPE 0x17
-#define CONSOLE_ADDR_WRITE 0x18
-#define CONSOLE_ADDR_ERROR 0x19
-#define CONSOLE_ADDR_ADDR 0x1c
-#define CONSOLE_ADDR_MODE 0x1e
-#define CONSOLE_ADDR_EXEC 0x1f
+#define CONSOLE_VECTOR_PORT 0x10
+#define CONSOLE_READ_PORT 0x12
+#define CONSOLE_TYPE_PORT 0x17
+#define CONSOLE_WRITE_PORT 0x18
+#define CONSOLE_ERROR_PORT 0x19
+#define CONSOLE_ADDR_PORT 0x1c
+#define CONSOLE_MODE_PORT 0x1e
+#define CONSOLE_EXEC_PORT 0x1f
 
 #define CONSOLE_TYPE_NO_QUEUE 0
 #define CONSOLE_TYPE_STDIN 1
@@ -16,4 +16,5 @@
 #define CONSOLE_TYPE_ARG_SPACER 3
 #define CONSOLE_TYPE_ARG_END 4
 
+int console_input_event(Uxn* uxn, Byte c, Byte type);
 void console_deo(Uxn* uxn, Byte addr);
