292c636d6140a8f126288ad4da0b45d5e1871451
John Honaker <honaker215@gmail.com>
2024-11-26 14:04:11 -0500

    Move Raylib-specific controller/keyboard code from main.c

---
+++ src/device/raylib/controller.h
@@ -0,0 +1,10 @@
+#include "../../common.h"
+#include "../../uxn.h"
+#include <raylib.h>
+
+#ifndef raylib_controller_h
+#define raylib_controller_h
+
+void controller_poll(Uxn *uxn);
+
+#endif // raylib_controller_h
